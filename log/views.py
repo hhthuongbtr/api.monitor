53a795727dc5f4b0d153a84c1b6be753c6e3af20
root <root@localhost.localdomain>
2017-06-13 20:18:31 -0700

    rabbit modurn for log app

--- log/views.py
+++ log/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from setting.customSQL import *
+from setting.rabbitmq_queue import *
 
 
 #######################################################################
@@ -25,7 +26,7 @@ class LogList(APIView):
         data=request.data
         if len(data)==3 and ('host' and 'tag' and 'msg' in data):
             querry="insert into logs(host,tag,datetime,msg) values('%s','%s', NOW(),'%s');"%(data['host'],data['tag'],data['msg'])
-            File().write(querry)
+            RabbitMQQueue().push_query(querry)
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
         #serializer = LogSerializer(data=request.data)
